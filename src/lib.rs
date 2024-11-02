aaf58676a2ee98ed12e80229a873c9a20a5168f9
Victor Teo <victor.teo@fillmula.com>
2024-11-02 21:19:20 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -4,20 +4,20 @@ extern crate proc_macro;
 use quote::ToTokens;
 use syn::{parse_quote, ItemFn};
 
-#[proc_macro_attribute]
-pub fn runtime(_args: proc_macro::TokenStream, input_stream: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let input_stream: proc_macro2::TokenStream = input_stream.into();
-    let mut input: ItemFn = match syn::parse2(input_stream.clone()) {
-        Ok(it) => it,
-        Err(_) => panic!("The `runtime` macro attribute is only valid when called on a fn."),
-    };
-    input.sig.asyncness = None;
-    let block = input.block;
-    input.block = parse_quote! {
-        shared_tokio_runtime::rt().block_on(async {
-            #block
-        })
-    };
-    let token_stream: proc_macro2::TokenStream = input.into_token_stream();
-    token_stream.into()
+#[proc_macro]
+pub fn runtime(input_stream: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    input_stream
+    // let input_stream: proc_macro2::TokenStream = input_stream.into();
+    // let mut input: ItemFn = match syn::parse2(input_stream.clone()) {
+    //     Ok(it) => it,
+    //     Err(_) => panic!("The `runtime` macro attribute is only valid when called on a fn."),
+    // };
+    // input.sig.asyncness = None;
+    // let block = input.block;
+    // input.block = parse_quote! {
+    //     shared_tokio_runtime::rt().block_on(async {
+    //         #block
+    //     })
+    // };
+    // input.into_token_stream().into()
 }
